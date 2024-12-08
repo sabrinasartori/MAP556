bcc026464b75a281653da0229af19c2a7ef6a7e1
sabrinasartori <sabrinasartori10@gmail.com>
2024-12-08 10:29:58 +0100

    update tp 5 and tp 6

--- TP6/metropolis_code_student.py
+++ TP6/metropolis_code_student.py
@@ -15,11 +15,16 @@ def random_walk_metropolis(num_samples, proposal_sd, x0):
     current_x = x0
     for i in range(num_samples):
         # Generate proposals
-        
+        proposal = current_x + np.random.normal(0, proposal_sd, size=dimx)
         # Compute acceptance probabilities
-        
+        curr_target = np.exp(-potential_function(current_x))
+        prop_target = np.exp(-potential_function(proposal))
+        accept_proba = min(1, prop_target/curr_target)
         # Accept proposals
-        
+        if np.random.rand() < accept_proba:
+            acceptance[i+1] = 1
+            current_x = proposal
+        samples[i] = current_x
 
     acceptance_rate = acceptance[1:]
     return samples, acceptance_rate 
@@ -32,11 +37,18 @@ def independance_metropolis(num_samples, proposal_sd, x0):
     current_x = x0
     for i in range(num_samples):
         # Generate proposals
-        
+        proposal = np.random.normal(0, proposal_sd, size=dimx)
         # Compute acceptance probabilities
-        
+        curr_target = np.exp(-potential_function(current_x))
+        prop_target = np.exp(-potential_function(proposal))
+        curr_q = np.exp(-np.sum(current_x)**2/(2*proposal_sd))
+        prop_q = np.exp(-np.sum(proposal)**2/(2*proposal_sd))
+        accept_proba = min(1, prop_target/curr_target * curr_q/prop_q)
         # Accept proposals
-        
+        if np.random.rand() < accept_proba:
+            acceptance[i+1] = 1
+            current_x = proposal
+        samples[i] = current_x
         
     acceptance_rate = acceptance[1:]
     return samples, acceptance_rate
